172c8820d3a36aba682e7b768f2bf31dfb8d05c6
Hoshin <lle21cen@naver.com>
2018-11-28 17:15:30 +0900

    마이 페이지 썸네일 표시, 열람목록 내부db 오류 수정, 이미 본 컨텐츠 list 항목의 배경 색을 회색으로 바꾸는 작업 보류

--- app/src/main/java/org/techtown/ideaconcert/SQLiteDir/RecentViewData.java
+++ app/src/main/java/org/techtown/ideaconcert/SQLiteDir/RecentViewData.java
@@ -5,12 +5,14 @@ public class RecentViewPair {
     private String contents_name;
     private String date;
     private int contents_num;
+    private String url;
 
-    public RecentViewPair(int contents_pk, String contents_name, String date, int contents_num) {
+    public RecentViewPair(int contents_pk, String contents_name, String date, int contents_num, String url) {
         this.contents_pk = contents_pk;
         this.contents_name = contents_name;
         this.date = date;
         this.contents_num = contents_num;
+        this.url = url;
     }
 
     public int getContents_pk() {
@@ -27,5 +29,9 @@ public class RecentViewPair {
 
     public int getContents_num() {
         return contents_num;
+    }
+
+    public String getUrl() {
+        return url;
     }
 }
