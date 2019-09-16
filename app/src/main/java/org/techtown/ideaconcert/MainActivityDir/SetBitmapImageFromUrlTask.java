37c9014a40451d734185af3c5d09a7ee3a245fc1
LeeHoShin <lle21cen@naver.com>
2019-09-16 23:03:41 +0900

    Update SetBitmapImageFromUrlTask.java

--- app/src/main/java/org/techtown/ideaconcert/MainActivityDir/SetBitmapImageFromUrlTask.java
+++ app/src/main/java/org/techtown/ideaconcert/MainActivityDir/SetBitmapImageFromUrlTask.java
@@ -36,24 +36,21 @@ public class SetBitmapImageFromUrlTask extends AsyncTask<String, Void, Bitmap> {
             connection.connect();
             InputStream in = connection.getInputStream();
             BitmapFactory.Options options = new BitmapFactory.Options(); // option 사이즈 width, height 모두 0 나옴
-
-            Bitmap src = BitmapFactory.decodeStream(in, new Rect(0, 0, 0, 0), options);
-
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(in, new Rect(0, 0, 0, 0), options);
+            
             if (dstHeight == 0) {
                 options.inSampleSize = 2;
-                options.inJustDecodeBounds = true;
-                dstHeight = options.outHeight * dstWidth / options.outWidth;
-                Log.i("이미지크기", "out width = " + options.outWidth + " out height = " + options.outHeight + " dstwidth = " + dstWidth + " dstHeight = " + dstHeight);
+            } else {
+                while(requestWidth < dstWidth){
+                    dstWidth = dstWidth / 2;
+                    size = size * 2;
+                }
+                options.inSampleSize = size;
             }
-
-            if (src != null) {
-                image = Bitmap.createScaledBitmap(src, dstWidth, dstHeight, true);
-            }
-
-//            if (!src.isRecycled()) {
-//                src.recycle();
-//            }
-
+            
+            options.inJustDecodeBounds = false;
+            image = BitmapFactory.decodeStream(in, new Rect(0, 0, 0, 0), options);
             connection.disconnect();
         } catch (MalformedURLException e) {
             Log.e("url형식오류", e.getMessage());
