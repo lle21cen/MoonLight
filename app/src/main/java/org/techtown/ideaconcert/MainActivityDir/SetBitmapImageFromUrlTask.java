e64794af40352e27326e4bb80871ba67e36299bd
LeeHoShin <lle21cen@naver.com>
2019-09-17 10:09:46 +0900

    Update SetBitmapImageFromUrlTask.java

--- app/src/main/java/org/techtown/ideaconcert/MainActivityDir/SetBitmapImageFromUrlTask.java
+++ app/src/main/java/org/techtown/ideaconcert/MainActivityDir/SetBitmapImageFromUrlTask.java
@@ -29,6 +29,7 @@ public class SetBitmapImageFromUrlTask extends AsyncTask<String, Void, Bitmap> {
     protected Bitmap doInBackground(String... urls) {
         String urlStr = urls[0];
         Bitmap image = null;
+        int size = 1;
         try {
             URL url = new URL(ActivityCodes.DATABASE_IP + urlStr);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -38,6 +39,7 @@ public class SetBitmapImageFromUrlTask extends AsyncTask<String, Void, Bitmap> {
             BitmapFactory.Options options = new BitmapFactory.Options(); // option 사이즈 width, height 모두 0 나옴
             options.inJustDecodeBounds = true;
             BitmapFactory.decodeStream(in, new Rect(0, 0, 0, 0), options);
+            int requestWidth = options.outWidth;
             
             if (dstHeight == 0) {
                 options.inSampleSize = 2;
